1eca7fbba68bd7f04592a2a1f4aeebdf09043f8b
soubhagya pradhan <soubhagya@monetree.co>
2022-06-30 16:23:18 +0530

    adde chanegs

--- src/components/Assessment/Assessment.js
+++ src/components/Assessment/Assessment.js
@@ -1,48 +1,110 @@
-import React from 'react';
-import './Assessment.css';
-import { assessments } from '../../data';
-
+import React, { useState } from "react";
+import "./Assessment.css";
+import { useQuery, gql } from "@apollo/client";
+import PieChart from "../charts/pieChart";
+import DatatablePage from "../charts/dataTable";
 
 const Assessment = () => {
-    return (
-        <div>
-            <h2 className='mb-4'>All Assessments</h2>
+  const QUERIES = gql`
+    query {
+      getAssessments(teacherRef: "62a827022ed38fb4323c53cd") {
+        id
+        subject
+        name
+        grade
+        published
+      }
 
-            <table className="dataTable">
-                <colgroup>
-                    <col style={{ width: '40%' }} />
-                    <col style={{ width: '25%' }} />
-                    <col style={{ width: '15%' }} />
-                    <col style={{ width: '20%' }} />
-                </colgroup>
+      getAssessmentScorecard(assessmentRef: "62aee40f28539421a30a8664") {
+        userRef
+        id
+        completed
+        createdAt
+        updatedAt
+        failed
+        passed
+        skipped
+      }
+    }
+  `;
 
-                <thead>
-                    <tr>
-                        <th scope="col">Assessment</th>
-                        <th scope="col">Subject</th>
-                        <th scope="col">Grade</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        assessments.map(item => {
-                            return (
-                                <tr key={item.id}>
-                                    <td>{item.name}</td>
-                                    <td>{item.subject}</td>
-                                    <td>{item.grade}</td>
-                                    <td className='status'>
-                                        <span className={(item.status === 'Draft' ? 'draft' : item.status === 'Completed' ? 'complete' : 'in-progress').concat(' rounded-pill py-1')}>{item.status}</span>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+  const { loading, error, data } = useQuery(QUERIES);
+  const [assessmentId, setAssessmentId] = useState(null);
+
+  return (
+    <div>
+      <h2 className="mb-4">All Assessments</h2>
+
+      {assessmentId ? (
+        <>
+          <div className="assessment-card">
+            <div className="row">
+              <div className="col-sm-6">
+                <h6>Assessment 3</h6>
+                <h6 className="mb-5">Biology | Grade 7 | 60 Mins</h6>
+
+                <h6>Published at: 12:22 AM, 23 June 2022</h6>
+                <h6>Assessment is in-progress</h6>
+              </div>
+              <div className="col-sm-6">
+                <PieChart />
+              </div>
+            </div>
+          </div>
+
+          {data && data.getAssessmentScorecard ? (
+            <DatatablePage data_={data.getAssessmentScorecard} />
+          ) : (
+            ""
+          )}
+        </>
+      ) : (
+        <table className="dataTable">
+          <colgroup>
+            <col style={{ width: "40%" }} />
+            <col style={{ width: "25%" }} />
+            <col style={{ width: "15%" }} />
+            <col style={{ width: "20%" }} />
+          </colgroup>
+
+          <thead>
+            <tr>
+              <th scope="col">Assessment</th>
+              <th scope="col">Subject</th>
+              <th scope="col">Grade</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          {data && data.getAssessments ? (
+            <tbody>
+              {data.getAssessments.map((item) => {
+                return (
+                  <tr key={item.id} onClick={() => setAssessmentId(item.id)}>
+                    <td>{item.name}</td>
+                    <td>{item.subject}</td>
+                    <td>{item.grade}</td>
+                    <td className="status">
+                      <span
+                        className={
+                          item.published
+                            ? "rounded-pill py-1 complete"
+                            : "draft"
+                        }
+                      >
+                        {item.published ? "Completed" : "Draft"}
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          ) : (
+            <tbody></tbody>
+          )}
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default Assessment;
---
+++ src/components/charts/dataTable.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { MDBDataTable } from "mdbreact";
+
+const DatatablePage = ({ data_ }) => {
+  const data = {
+    columns: [
+      {
+        label: "userRef",
+        field: "userRef",
+        sort: "asc",
+        width: 150,
+      },
+      //   {
+      //     label: "completed",
+      //     field: "completed",
+      //     sort: "asc",
+      //     width: 270,
+      //   },
+      {
+        label: "createdAt",
+        field: "createdAt",
+        sort: "asc",
+        width: 200,
+      },
+      {
+        label: "updatedAt",
+        field: "updatedAt",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "failed",
+        field: "failed",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "passed",
+        field: "passed",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "skipped",
+        field: "skipped",
+        sort: "asc",
+        width: 100,
+      },
+    ],
+    rows: [data_],
+  };
+
+  return <MDBDataTable striped bordered small data={data} />;
+};
+
+export default DatatablePage;
