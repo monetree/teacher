5436a4274809920a3646f94c82dbc6ba16a5d95a
Tanveerhusyn <tanveerhussain465@gmail.com>
2022-07-09 21:32:08 +0500

    editor with mathtype

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1,21 +1,73 @@
-import React from "react";
-import TinyMCE from "react-tinymce";
+import React, { Component } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "ckeditor5-build-classic-mathtype";
 
-const Editor = () => {
-  const handleEditorChange = (e) => {
-    console.log(e.target.getContent());
-  };
-
-  return (
-    <TinyMCE
-      content="<p>This is the initial content of the editor</p>"
-      config={{
-        plugins: "autolink link image lists print preview",
-        toolbar: "undo redo | bold italic | alignleft aligncenter alignright",
-      }}
-      onChange={handleEditorChange}
-    />
-  );
-};
+class Editor extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h4>Welcome to the editor</h4>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>This is the content area</p>"
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+          config={{
+            toolbar: {
+              shouldNotGroupWhenFull: true,
+              items: [
+                // 'heading', '|',
+                // 'fontfamily', 'fontsize', '|',
+                // 'alignment', '|',
+                // 'fontColor', 'fontBackgroundColor', '|',
+                // 'bold', 'italic', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
+                // 'link', '|',
+                // 'outdent', 'indent', '|',
+                // 'bulletedList', 'numberedList', 'todoList', '|',
+                // 'code', 'codeBlock', '|',
+                // 'insertTable', '|',
+                // 'uploadImage', 'blockQuote', '|',
+                "heading",
+                "fontsize",
+                "alignment",
+                "fontColor",
+                "fontBackgroundColor",
+                "outdent",
+                "indent",
+                "|",
+                "bold",
+                "italic",
+                "link",
+                "bulletedList",
+                "numberedList",
+                "imageUpload",
+                "mediaEmbed",
+                "insertTable",
+                "blockQuote",
+                "undo",
+                "redo",
+                "|",
+                "MathType",
+                "ChemType"
+              ]
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Editor;
